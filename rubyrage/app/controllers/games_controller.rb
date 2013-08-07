e233246981e8229469bc14098860d8da6c996b9f
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-08-07 14:52:52 -0400

    Rerouted index page to link to newly created game dashboard

---
+++ rubyrage/app/controllers/games_controller.rb
@@ -0,0 +1,7 @@
+class GamesController < ApplicationController
+
+  def dashboard
+
+  end
+
+end
--- rubyrage/config/routes.rb
+++ rubyrage/config/routes.rb
@@ -1,6 +1,8 @@
 Rubyrage::Application.routes.draw do
  root :to => 'welcome#index'
  resources :users #CHANGE
+  get "log_out" => "sessions#destroy", as: "log_out"
  resources :sessions #CHANGE
+ get '/games/dashboard' => 'games#dashboard'
 
  end
