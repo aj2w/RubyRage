d0fc5fe45857eaf0dde5964992a25230a4e161bc
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-08-08 12:47:43 -0400

    removed deleted files

--- rubyrage/app/models/stat.rb
+++
@@ -1,8 +0,0 @@
-class Stat < ActiveRecord::Base
-  attr_accessible :times_played, :total_score, :user_id
-  belongs_to :user
-
-  # validates :text, :rating, presence: true
-  # validates :rating, numericality: true
-
-end
