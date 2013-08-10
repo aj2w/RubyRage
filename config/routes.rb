d8db783c260001866382ecef535878b7239f5865
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-08-10 15:02:18 -0400

    working on onload function to work

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rubyrage::Application.routes.draw do
 
  get '/games/dashboard' => 'games#dashboard'
  get '/games/single' => 'games#single'
- get '/games/multi/requestbattle' => 'games#requestBattle', as: "requestbattle"
+ post '/games/multi/requestBattle' => 'games#requestBattle', as: "requestbattle"
  get '/games/multi/:username' => 'games#multi', as: "games_multi"
  get '/games/pusher' => 'games#pusher'
  post '/games/gameBroadcast' => 'games#gameBroadcast'
