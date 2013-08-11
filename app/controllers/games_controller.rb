154a499fad6ccf24a237f78c688a405bea35c851
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-08-11 16:55:17 -0400

    Thomas's updated game controller code

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -23,22 +23,46 @@ class GamesController < ApplicationController
     myCurrentXposition        = params[:myCurrentXposition]
     myCurrentYposition        = params[:myCurrentYposition]
     myUpcomingRubymonPair     = params[:myUpcomingRubymonPair]
-    Pusher[opponentChannelName].trigger('gamebroadcast', {
-      mySittingRubymonsPosition: mySittingRubymonsPosition,
-      myFallingRubymonsPosition: myFallingRubymonsPosition,
-      myCurrentXposition: myCurrentXposition,
-      myCurrentYposition: myCurrentYposition,
-      myUpcomingRubymonPair: myUpcomingRubymonPair
+
+    respond_to do |format|
+      # format.html
+      json_response = Pusher[opponentChannelName].trigger('gamebroadcast', {
+        mySittingRubymonsPosition: mySittingRubymonsPosition,
+        myFallingRubymonsPosition: myFallingRubymonsPosition,
+        myCurrentXposition: myCurrentXposition,
+        myCurrentYposition: myCurrentYposition,
+        myUpcomingRubymonPair: myUpcomingRubymonPair
       })
+      format.json { render :json => json_response }
+    end
   end
+
+  # opponentChannelName = params[:opponentChannelName]
+  # mySittingRubymonsPosition = params[:mySittingRubymonsPosition].values
+  # myFallingRubymonsPosition = params[:myFallingRubymonsPosition].values
+  # myCurrentXposition        = params[:myCurrentXposition]
+  # myCurrentYposition        = params[:myCurrentYposition]
+  # myUpcomingRubymonPair     = params[:myUpcomingRubymonPair]
+  # Pusher[opponentChannelName].trigger('gamebroadcast', {
+  #   mySittingRubymonsPosition: mySittingRubymonsPosition,
+  #   myFallingRubymonsPosition: myFallingRubymonsPosition,
+  #   myCurrentXposition: myCurrentXposition,
+  #   myCurrentYposition: myCurrentYposition,
+  #   myUpcomingRubymonPair: myUpcomingRubymonPair
+  #   })
+
 
   def requestBattle
     opponentChannelName = params[:opponentChannelName]
     challengerChannelName = params[:challengerChannelName]
-    Pusher[opponentChannelName].trigger('promptRequestPopup', {
-      message: "#{@current_user.username} has requested a battle with you!",
-      challengerChannelName: challengerChannelName
+
+    respond_to do |format|
+      json_response = Pusher[opponentChannelName].trigger('promptRequestPopup', {
+        message: "#{@current_user.username} has requested a battle with you!",
+        challengerChannelName: challengerChannelName
       })
+      format.json { render :json => json_response }
+    end
   end
 
   def challengeResponse
@@ -49,23 +73,27 @@ class GamesController < ApplicationController
     else
       messageToChallenger = "'#{@current_user.username}' has rejected your challenge. \nClick 'Ok' to go back to dashboard."
     end
-    Pusher[opponentChannelName].trigger('challengeResponse', {
+
+    respond_to do |format|
+      json_response = Pusher[opponentChannelName].trigger('challengeResponse', {
       message: messageToChallenger,
       responseToChallenge: responseToChallenge,
       opponentChannelName: @current_user.username
       })
+      format.json { render :json => json_response }
+    end
   end
 
   def triggerBattleGame
-    # commonChannelName = params[:commonChannelName]
     myOwnChannelName = params[:myOwnChannelName]
     opponentChannelName = params[:opponentChannelName]
-    binding.pry
-    Pusher[myOwnChannelName].trigger('startCountdownAndGo', {message: "Start game!"})
-    Pusher[opponentChannelName].trigger('startCountdownAndGo', {message: "Start game!"})
+
+    respond_to do |format|
+      json_response_one = Pusher[myOwnChannelName].trigger('startCountdownAndGo', {message: "Start game!"})
+      json_response_two = Pusher[opponentChannelName].trigger('startCountdownAndGo', {message: "Start game!"})
+      json_response_total = { :json_response_one => json_response_one, :json_response_two => json_response_two }
+      format.json { render :json => json_response_total }
+    end
   end
 
 end
-
-
-
