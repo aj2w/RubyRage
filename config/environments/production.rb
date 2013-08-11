8fd2d17a95e05d5ae29410006b8c08771e8b03cf
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-08-11 13:38:01 -0400

    Rubyrage set to precompile in environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Rubyrage::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.precompile += %w( rubyrage.js )
 
   # Generate digests for assets URLs
   config.assets.digest = true
