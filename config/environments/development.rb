120a616b9065d34e618a820362514ad083bcaf0b
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-08-11 19:02:09 -0400

    Changed production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rubyrage::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 
   Pusher.app_id = '51516'
   Pusher.key    = 'd09eb96f1aef0dea4b31'
