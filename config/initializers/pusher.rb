369220851144decb5d184f2e7421095b421c42b4
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-08-12 00:30:26 -0400

    revision

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,14 @@
+# Pusher.app_id = '51011'
+# Pusher.key = '6263345b4b07e260d959'
+# Pusher.secret = 'd13ee8fffb60d88a3ecc'
+
+
+# Pusher.app_id = '51341'
+# Pusher.key = '3b6a36daa604f4b385f9'
+# Pusher.secret = '6f1845bf67c4e845c17b'
+
+require 'pusher'
+
+Pusher.url = "http://d55ecc1d9e47eb42ed12:fbde5be976cde7a54ccd@api.pusherapp.com/apps/51011"
+Pusher.logger = Rails.logger
+
